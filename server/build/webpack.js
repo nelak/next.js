64dcb04449ec84b02778270bd302bf740fd96e60
Gaston Cababie <nelak@e107.com.ar>
2017-08-01 10:46:36 -0300

    Added support for multiple extensions

--- server/build/webpack.js
+++ server/build/webpack.js
@@ -47,7 +47,7 @@ export default async function createCompiler (dir, { dev = false, quiet = false,
       ]
     }
 
-    const pages = await glob('pages/**/*.{js,ts,tsx}', { cwd: dir })
+    const pages = await glob(`pages/**/*.{${config.pagesExtensions.join(',')}}`, { cwd: dir })
     const devPages = pages.filter((p) => p === 'pages/_document.js' || p === 'pages/_error.js')
 
     // In the dev environment, on-demand-entry-handler will take care of
@@ -184,15 +184,17 @@ export default async function createCompiler (dir, { dev = false, quiet = false,
     mainBabelOptions.presets.push(require.resolve('./babel/preset'))
   }
 
+  const pagesRegex = new RegExp(`\\.(${config.pagesExtensions.join('|')})(\\?[^?]*)?$`)
+
   const rules = (dev ? [{
-    test: /\.(js|tsx?)(\?[^?]*)?$/,
+    test: pagesRegex,
     loader: 'hot-self-accept-loader',
     include: [
       join(dir, 'pages'),
       nextPagesDir
     ]
   }, {
-    test: /\.(js|tsx?)(\?[^?]*)?$/,
+    test: pagesRegex,
     loader: 'react-hot-loader/webpack',
     exclude: /node_modules/
   }] : [])
@@ -296,6 +298,7 @@ export default async function createCompiler (dir, { dev = false, quiet = false,
       chunkFilename: '[name]'
     },
     resolve: {
+      extensions: config.pagesExtensions.map(ext => `.${ext}`),
       modules: [
         nextNodeModulesDir,
         'node_modules',
